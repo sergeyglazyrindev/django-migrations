69c823a80b320dd4eb77060659406bce9bbf68e2
Sergey Glazyrin <sergey.glazyrin.dev@gmail.com>
2014-04-14 14:35:18 +0300

    changed project name

--- migrations/settings.py
+++ migrations/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for migrations project.
+Django settings for django-migrations project.
 
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/topics/settings/
@@ -48,9 +48,9 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
 
-ROOT_URLCONF = 'migrations.urls'
+ROOT_URLCONF = 'django-migrations.urls'
 
-WSGI_APPLICATION = 'migrations.wsgi.application'
+WSGI_APPLICATION = 'django-migrations.wsgi.application'
 
 
 # Database
